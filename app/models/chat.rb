e974f7e7b2a4ad04b8e8a9009f79820001d228e9
Omar Hawas <omarhawas94@gmail.com>
2024-09-16 12:21:50 -0400

    adding all models and associations. added bcrypt

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,2 +1,6 @@
 class Chat < ApplicationRecord
+    has_many :messages
+    has_many :chat_users
+    has_many :users, through: :chat_users
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :messages
+  has_many :chat_users
+  has_many :chats, through: :chat_users
 end
